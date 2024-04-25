f771868c453391b39512041b6ab196b6930e0d90
Li-yao Xia <lysxia@gmail.com>
2024-04-25 14:48:29 +0200

    BluefinOverEffectful.interpret

--- src/Toro/BluefinOverEffectful.hs
+++ src/Toro/BluefinOverEffectful.hs
@@ -7,11 +7,27 @@
   MultiParamTypeClasses,
   PolyKinds,
   RankNTypes,
+  TypeFamilies,
   TypeOperators #-}
-module Toro.BluefinOverEffectful (Eff, runPure, type (:&), type (:>), State, runState, get, put) where
+module Toro.BluefinOverEffectful
+  ( Eff
+  , runPure
+  , type (:&)
+  , type (:>)
+  , State
+  , runState
+  , get
+  , put
+  , FEffect
+  , HandleOf
+  , interpret
+  , example
+  ) where
 
+import Data.Kind (Type)
 import Data.Type.Equality
 import qualified Effectful as E
+import qualified Effectful.Dispatch.Dynamic as E
 import qualified Effectful.State.Dynamic as E
 
 newtype Eff ε a = Eff { unEff :: E.Eff ε a }
@@ -36,6 +52,19 @@ get (State Refl) = Eff E.get
 
 put :: η :> ε => State s η -> s -> Eff ε ()
 put (State Refl) s = Eff (E.put s)
+
+-- First-order effect
+newtype FEffect (f :: Type -> Type) (m :: Type -> Type) (a :: Type) = FEffect (f a)
+
+type instance E.DispatchOf (FEffect f) = 'E.Dynamic
+
+newtype HandleOf (f :: Type -> Type) (η :: E.Effect) = HandleOf (η :~: FEffect f)
+
+interpret ::
+  (forall x. f x -> Eff ε x) ->
+  (forall η. HandleOf f η -> Eff (η :& ε) a) ->
+  Eff ε a
+interpret g m = Eff (E.interpret (\_ (FEffect e) -> unEff (g e)) (unEff (m (HandleOf Refl))))
 
 example :: Eff ε Int
 example = do
