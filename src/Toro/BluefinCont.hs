3a241fadca9fce117b1b64a0af3e186ca5a8e438
Li-yao Xia <lysxia@gmail.com>
2024-04-25 19:01:39 +0200

    doc

--- src/Toro/BluefinCont.hs
+++ src/Toro/BluefinCont.hs
@@ -6,10 +6,35 @@
   StandaloneKindSignatures,
   TypeOperators,
   UnboxedTuples #-}
+
+-- * Delimited continuations
+--
+-- A thin wrapper around GHC's
+-- <native delimited continuations primitives https://ghc-proposals.readthedocs.io/en/latest/proposals/0313-delimited-continuation-primops.html>.
+--
+-- Those primitives let us manipulate slices of the call stack/evaluation
+-- context delimited by prompts.
+--
+-- We use this module to implement algebraic effect handlers, which provide a more
+-- structured interface for manipulating continuations.
+--
+-- The behavior of 'prompt' and 'control' is summarized by the following equations:
+--
+-- @
+-- 'prompt' (\\_ -> 'pure' x) = 'pure' x
+-- 'prompt' (\\t -> C ('control' t f)) = f (\\x -> 'prompt' (\\t -> C x))
+-- @
+--
+-- where @C@ is an evaluation context (in which @t@ may occur),
+-- a term of the following form:
+--
+-- > C x ::= C x >>= f      -- for any f
+-- >       | H (\h -> C x)  -- for any bluefin handler H ∈ { prompt, (`runState` s), ... }
+-- >       | x
 module Toro.BluefinCont
-  ( Prompt
+  ( PromptTag
   , prompt
-  , control0
+  , control
   ) where
 
 import Data.Kind (Type)
@@ -18,8 +43,9 @@ import GHC.IO (IO(IO))
 import Bluefin.Internal (Eff(..))
 import Bluefin.Eff
 
-type Prompt :: Effects -> Type -> Effects -> Type
-data Prompt ss a s = Prompt (PromptTag# a)
+-- | Tag for a prompt of type @a@.
+type PromptTag :: Effects -> Type -> Effects -> Type
+data PromptTag ss a s = PromptTag (PromptTag# a)
 
 unsafeMkEff :: (State# RealWorld -> (# State# RealWorld , a #)) -> Eff ss a
 unsafeMkEff f = UnsafeMkEff (IO f)
@@ -27,11 +53,25 @@ unsafeMkEff f = UnsafeMkEff (IO f)
 unsafeRunEff :: Eff ss a -> State# RealWorld -> (# State# RealWorld , a #)
 unsafeRunEff (UnsafeMkEff (IO f)) = f
 
-prompt :: forall a ss. (forall s. Prompt ss a s -> Eff (s :& ss) a) -> Eff ss a
+-- | Push a tagged prompt of type @a@ on the stack and pass it to the tagged computation.
+--
+-- A prompt is a marker on the call stack (evaluation context).
+-- Those markers delimit slices of the call stack which can be removed from the
+-- stack with 'control'. Such a slice is copied as a value on the heap,
+-- a continuation, which is a function of type @Eff ss b -> Eff ss0 b@.
+-- Calling the continuation restores the slice on the stack.
+prompt :: forall a ss.
+  (forall s. PromptTag ss a s -> Eff (s :& ss) a) ->
+  Eff ss a
 prompt f = unsafeMkEff (\z0 -> case newPromptTag# z0 of
-    (# z1, tag #) -> prompt# tag (unsafeRunEff (f (Prompt tag))) z1)
+    (# z1, tag #) -> prompt# tag (unsafeRunEff (f (PromptTag tag))) z1)
 
-control0 :: forall s a b ss ss0. Prompt ss0 a s -> ((Eff ss b -> Eff ss0 a) -> Eff ss0 a) -> Eff ss b
-control0 (Prompt tag) f = unsafeMkEff (\z0 ->
+-- | Capture the continuation up to the tagged prompt.
+-- The prompt is reinserted on the stack when the continuation is called.
+control :: forall s a b ss ss0.
+  PromptTag ss0 a s ->
+  ((Eff ss b -> Eff ss0 a) -> Eff ss0 a) ->
+  Eff ss b
+control (PromptTag tag) f = unsafeMkEff (\z0 ->
   control0# tag (\k# ->
-    unsafeRunEff (f (unsafeMkEff . k# . unsafeRunEff))) z0)
+    unsafeRunEff (f (unsafeMkEff . prompt# tag . k# . unsafeRunEff))) z0)
--- src/Toro/BluefinHandler.hs
+++ src/Toro/BluefinHandler.hs
@@ -5,16 +5,87 @@
   ScopedTypeVariables,
   StandaloneKindSignatures,
   TypeOperators #-}
-module Toro.BluefinHandler where
 
+-- * Algebraic effects and named handlers
+--
+-- Algebraic effect handlers are a powerful framework for
+-- user-defined effects with a simple equational intuition.
+--
+-- An "algebraic effect" is a signature for a set of operations which we
+-- represent with a GADT. For example, the "state effect" @State s@ contains
+-- two operations: @Get@ takes no parameter and returns a value of type @s@,
+-- and @Put@ takes a value of type @s@ and returns @()@. The constructors
+-- @Get@ and @Put@ are "uninterpreted operations": they only describe the types
+-- of arguments and results, with no further intrinsic meaning.
+--
+-- @
+-- data State s r where
+--   Get :: State s s
+--   Put :: s -> State s ()
+-- @
+--
+-- Below is an example of a stateful computation: a term of some type @'Eff' ss a@ with
+-- a state handler @h :: 'Handler' (State s) z@ in scope (@z :> zz@).
+-- The @State@ operations can be called using 'call' and the state handler @h@.
+--
+-- @
+-- -- Increment a counter and return its previous value.
+-- incr :: s :> ss => Handler (State Int) s -> Eff ss Int
+-- incr h = do
+--     n <- get
+--     put (n + 1)
+--     pure n
+--   where
+--     get = call h Get
+--     put s = call h (Put s)
+-- @
+--
+-- We handle the state effect by giving an interpretation of the @Get@ and @Put@
+-- operations, as a function @f :: 'HandlerBody' (State s) ss a@.
+--
+-- To 'call' @Get@ or @Put@ is to call the function @f@ supplied by 'handle'.
+-- The following equations show how 'handle' propagates an interpretation
+-- @f@ throughout a computation that calls @Get@ and @Put@:
+--
+-- @
+-- handle f (\\h -> call h Get     >>= k h) = f Get     (handle f (\\h -> k h))
+-- handle f (\\h -> call h (Put s) >>= k h) = f (Put s) (handle f (\\h -> k h))
+-- handle f (\\h -> pure r) = pure r
+-- @
+--
+-- For example, @'handle' f@ maps the above @incr@ computation to:
+--
+-- @
+-- handle f incr =
+--   f Get \\n ->
+--   f (Put (n+1)) \\() ->
+--   pure n
+-- @
+module Toro.BluefinHandler
+  ( AEffect
+  , HandlerBody
+  , Handler
+  , handle
+  , call
+  ) where
+
+import Data.Kind (Type)
 import Bluefin.Eff
 import Toro.BluefinCont
 
+-- | Algebraic effect.
+type AEffect = Type -> Type
+
+-- | Interpretation of an algebraic effect @f@: a function to handle the operations of @f@.
+type HandlerBody :: AEffect -> Effects -> Type -> Type
 type HandlerBody f ss a = (forall x. f x -> (x -> Eff ss a) -> Eff ss a)
 
+-- | Handler to call operations of the effect @f@.
+type Handler :: AEffect -> Effects -> Type
 data Handler f s where
-  Handler :: !(Prompt ss a s) -> HandlerBody f ss a -> Handler f s
+  Handler :: !(PromptTag ss a s) -> HandlerBody f ss a -> Handler f s
 
+-- | Handle operations of @f@.
 handle ::
   HandlerBody f ss a ->
   (forall s. Handler f s -> Eff (s :& ss) a) ->
@@ -22,6 +93,7 @@ handle ::
 handle h act =
   prompt (\p -> act (Handler p h))
 
+-- | Call an operation of @f@.
 call :: Handler f s -> f b -> Eff ss b
 call (Handler p h) op =
-  control0 p (\k -> h op (k . pure))
+  control p (\k -> h op (k . pure))
