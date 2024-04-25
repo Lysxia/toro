827da7318c1f6e1d0a18d95b9ad0e1fdbd2f879a
Li-yao Xia <lysxia@gmail.com>
2024-04-26 01:37:06 +0200

    A lot of doc. ExceptionDynamic and cancellable exceptions

---
+++ src/Toro/BluefinExceptionDynamic.hs
@@ -0,0 +1,50 @@
+{-# LANGUAGE
+  KindSignatures,
+  ScopedTypeVariables,
+  TypeOperators #-}
+module Toro.BluefinExceptionDynamic
+  ( DynExn
+  , ioeToDynExn
+  , throw
+  , catch
+  , bracket
+  , finally
+  , onException
+  , throwIO
+  , catchIO
+  ) where
+
+import qualified Control.Exception as E
+import qualified Bluefin.Internal as B
+import Bluefin.Eff
+import Bluefin.IO
+
+data DynExn (ex :: Effects) = DynExn
+
+ioeToDynExn :: IOE io -> DynExn io
+ioeToDynExn _ = DynExn
+
+throw :: (E.Exception e, ex :> es) => DynExn ex -> e -> Eff es a
+throw _ e = B.UnsafeMkEff (E.throwIO e)
+
+catch :: (E.Exception e, ex :> es) => DynExn ex -> Eff es a -> (e -> Eff es a) -> Eff es a
+catch _ m h = B.UnsafeMkEff (E.catch (B.unsafeUnEff m) (B.unsafeUnEff . h))
+
+bracket :: ex :> es => DynExn ex -> Eff es a -> (a -> Eff es ()) -> (a -> Eff es b) -> Eff es b
+bracket ex acquire release run = do
+  a <- acquire
+  finally ex (run a) (release a)
+
+finally :: ex :> es => DynExn ex -> Eff es a -> Eff es () -> Eff es a
+finally ex run cleanup =
+  onException ex run cleanup   -- if run throws an exception, then only this cleanup will run
+    <* cleanup                 -- if run does not throw, then only this cleanup will run
+
+onException :: ex :> es => DynExn ex -> Eff es a -> Eff es () -> Eff es a
+onException ex run cleanup = catch ex run (\(e :: E.SomeException) -> cleanup >> throw ex e)
+
+throwIO :: (E.Exception e, io :> es) => IOE io -> e -> Eff es a
+throwIO io = throw (ioeToDynExn io)
+
+catchIO :: (E.Exception e, io :> es) => IOE io -> Eff es a -> (e -> Eff es a) -> Eff es a
+catchIO io = catch (ioeToDynExn io)
--- src/Toro/BluefinHandler.hs
+++ src/Toro/BluefinHandler.hs
@@ -6,7 +6,7 @@
   StandaloneKindSignatures,
   TypeOperators #-}
 
--- * Algebraic effects and named handlers
+-- | = Algebraic effects and named handlers
 --
 -- Algebraic effect handlers are a powerful framework for
 -- user-defined effects with a simple equational intuition.
@@ -16,21 +16,21 @@
 -- two operations: @Get@ takes no parameter and returns a value of type @s@,
 -- and @Put@ takes a value of type @s@ and returns @()@. The constructors
 -- @Get@ and @Put@ are "uninterpreted operations": they only describe the types
--- of arguments and results, with no further intrinsic meaning.
---
+-- of arguments and results, with no intrinsic meaning.
+-- 
 -- @
 -- data State s r where
 --   Get :: State s s
 --   Put :: s -> State s ()
 -- @
 --
--- Below is an example of a stateful computation: a term of some type @'Eff' ss a@ with
+-- Below is an example of a stateful computation: a term of some type @'Eff' zz a@ with
 -- a state handler @h :: 'Handler' (State s) z@ in scope (@z :> zz@).
 -- The @State@ operations can be called using 'call' and the state handler @h@.
 --
 -- @
 -- -- Increment a counter and return its previous value.
--- incr :: s :> ss => Handler (State Int) s -> Eff ss Int
+-- incr :: z :> zz => Handler (State Int) z -> Eff zz Int
 -- incr h = do
 --     n <- get
 --     put (n + 1)
@@ -41,7 +41,7 @@
 -- @
 --
 -- We handle the state effect by giving an interpretation of the @Get@ and @Put@
--- operations, as a function @f :: 'HandlerBody' (State s) ss a@.
+-- operations, as a function @f :: 'HandlerBody' (State s) zz a@.
 --
 -- To 'call' @Get@ or @Put@ is to call the function @f@ supplied by 'handle'.
 -- The following equations show how 'handle' propagates an interpretation
@@ -53,25 +53,43 @@
 -- handle f (\\h -> pure r) = pure r
 -- @
 --
--- For example, @'handle' f@ maps the above @incr@ computation to:
+-- With those equations, @'handle' f@ applied to the above @incr@ simplifies to:
 --
 -- @
--- handle f incr =
+-- 'handle' f incr =
 --   f Get \\n ->
 --   f (Put (n+1)) \\() ->
 --   pure n
 -- @
+--
+-- === References
+--
+-- - <https://www.microsoft.com/en-us/research/uploads/prod/2021/05/namedh-tr.pdf First-class names for effect handlers> (2021) by Ningning Xie, Youyou Cong, and Daan Leijen.
 module Toro.BluefinHandler
   ( AEffect
+
+    -- * Simple continuations
   , HandlerBody
   , Handler
   , handle
   , call
+
+    -- * Cancellable continuations
+  , HandlerBody'
+  , Handler'
+  , handle'
+  , call'
+  , continue
+  , discontinue
+  , discontinueIO
   ) where
 
+import Control.Exception (Exception)
 import Data.Kind (Type)
 import Bluefin.Eff
+import Bluefin.IO (IOE)
 import Toro.BluefinCont
+import Toro.BluefinExceptionDynamic
 
 -- | Algebraic effect.
 type AEffect = Type -> Type
@@ -86,14 +104,90 @@ data Handler f s where
   Handler :: !(PromptTag ss a s) -> HandlerBody f ss a -> Handler f s
 
 -- | Handle operations of @f@.
+--
+-- === Warning for exception-like effects
+--
+-- If the handler might not call the continuation (like for an exception effect), and
+-- if the handled computation manages resources (e.g., opening files, transactions)
+-- prefer 'handle'' to trigger resource clean up with cancellable continuations.
 handle ::
   HandlerBody f ss a ->
   (forall s. Handler f s -> Eff (s :& ss) a) ->
   Eff ss a
-handle h act =
-  prompt (\p -> act (Handler p h))
+handle h act = reset (\p -> act (Handler p h))
 
 -- | Call an operation of @f@.
-call :: Handler f s -> f b -> Eff ss b
-call (Handler p h) op =
-  control p (\k -> h op (k . pure))
+call :: s :> ss => Handler f s -> f a -> Eff ss a
+call (Handler p h) op = shift0 p (\k -> h op (continue k))
+
+-- | Variant of 'handle' with cancellable continuations.
+--
+-- This is useful to work with native exception handlers such as
+-- 'Control.Exception.bracket' and other resource-management schemes à la
+-- @resourcet@.
+--
+-- === Linearity
+--
+-- The continuation should be called exactly once (via 'continue' or 'discontinue')
+-- to avoid use-after-free errors.
+-- Enforcing this requirement with linear types would be a welcome contribution.
+--
+-- === Example
+--
+-- Given @bracket@ function (hypothetically adapted to bluefin) and a @Fail@ effect,
+-- the simple 'handle' may cause resource leaks:
+--
+-- @
+-- 'handle' (\\_e _k -> pure Nothing)
+--   (bracket acquire release (\\_ -> call h Fail))
+-- @
+--
+-- @bracket@ is intended to ensure that the acquired resource is released even if the bracketed
+-- function throws an exception. However, when the @Fail@ operation is called, the handler
+-- @(\_e _k -> pure Nothing)@ discards the continuation @_k@ which contains the
+-- exception handler installed by @bracket@.
+-- The resource leaks because @release@ will never be called.
+--
+-- The more general 'handle'' lets us cancel the continuation with 'discontinue'.
+-- Cancellable continuations require an 'IOE' handle.
+--
+-- @
+-- data CancelContinuation = CancelContinuation
+--   deriving (Show, Exception)
+--
+-- 'handle'' io (\\_e k -> try @CancelContinuation ('discontinueIO' k CancelContinuation) >> pure Nothing)
+--   (bracket acquire release (\\_ -> call h Fail))
+-- @
+handle' :: ex :> ss =>
+  h ex ->
+  HandlerBody' ex f ss a ->
+  (forall s. Handler' ex f s -> Eff (s :& ss) a) ->
+  Eff ss a
+handle' _ h act = reset (\p -> act (Handler' p h))
+
+-- | Variant of 'HandlerBody' with cancellable continuations (see 'handle'').
+type HandlerBody' :: Effects -> AEffect -> Effects -> Type -> Type
+type HandlerBody' ex f ss a = (forall x ss0. ex :> ss0 => f x -> (Eff ss0 x -> Eff ss a) -> Eff ss a)
+
+-- | Variant of 'Handler' with cancellable continuations (see 'handle'').
+type Handler' :: Effects -> AEffect -> Effects -> Type
+data Handler' ex f s where
+  Handler' :: !(PromptTag ss a s) -> HandlerBody' ex f ss a -> Handler' ex f s
+
+-- | Variant of 'call' with cancellable continuations (see 'handle'').
+call' :: (ex :> es, s :> es) => Handler' ex f s -> f a -> Eff es a
+call' (Handler' p h) op = shift0 p (\k -> h op k)
+
+-- | Resume a cancellable continuation with a result.
+--
+-- In other words, this converts a cancellable continuation to a simple continuation.
+continue :: (Eff ss0 b -> Eff ss a) -> b -> Eff ss a
+continue k = k . pure
+
+-- | Cancel a continuation: resume by throwing a (dynamic) exception.
+discontinue :: (Exception e, ex :> es0) => DynExn ex -> (Eff es0 b -> Eff es a) -> e -> Eff es a
+discontinue ex k e = k (throw ex e)
+
+-- | 'discontinue' with an 'IOE' handle instead of the more restrictive 'DynExn'.
+discontinueIO :: (Exception e, io :> es0) => IOE io -> (Eff es0 b -> Eff es a) -> e -> Eff es a
+discontinueIO io = discontinue (ioeToDynExn io)
